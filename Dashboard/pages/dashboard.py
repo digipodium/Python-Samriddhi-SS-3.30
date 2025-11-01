85ab24337f71f8abb5cca2733a386cf4442e7c65
Areeba Shakeel <shakeelareeba02@gmail.com>
2025-11-01 18:23:50 +0530

    first commit

---
+++ Dashboard/pages/dashboard.py
@@ -0,0 +1,47 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import plotly.express as px
+
+df = sns.load_dataset("titanic")
+
+st.title("Explore the insights here👇")
+
+st.dataframe(df)
+
+#filtering options
+st.sidebar.title("Filter Options")
+
+#gender filter
+gender = st.sidebar.multiselect("Gender",
+                                options=df["sex"].unique(),
+                                )
+
+#class filter
+pclass = st.sidebar.multiselect("Passenger Class",
+                                options=df["class"].unique(),
+                                )
+
+#age filter
+age = st.sidebar.slider("Age Range",
+                        min_value=int(df["age"].min()),
+                        max_value=int(df["age"].max()),
+                        value=(int(df["age"].min()), int(df["age"].max()))
+                        )
+
+#histogram of age distribution
+fig = px.histogram(df, x="age", nbins=30, title="Age Distribution")
+st.plotly_chart(fig)
+#conclusion point
+st.markdown("**Conclusion:** The age distribution of Titanic passengers is right-skewed, with a higher concentration of younger passengers. There is a noticeable drop in the number of passengers above 60 years old.")
+
+#bar chart of survival count by passenger class
+fig2 = px.bar(df, x = 'class', y='survived', 
+              labels={'index':'Survived', 'survived':'Count'}, 
+              title="Survival Count by Passenger Class")
+st.plotly_chart(fig2)
+
+#pie chart of class distribution
+fig3 = px.pie(df, names='class', title="Class Distribution")
+st.plotly_chart(fig3)
+
---
+++ calculator.py
@@ -0,0 +1,30 @@
+#streamlit run calculator.py
+import streamlit as st
+
+st.title("Simple Calculator")
+# st.subheader("Perform basic arithmetic operations")
+st.markdown("Welcome to my first web application")
+
+c1,c2 = st.columns(2)
+fnum = c1.number_input("Enter first number", value=0)
+snum = c2.number_input("Enter second number", value=0)
+
+options = ["Add", "Subtract", "Multiply", "Divide"]
+choice = st.radio("Select operation", options)
+
+button = st.button("Calculate")
+
+result = 0
+if button:
+    if choice == "Add":
+        result  = fnum + snum
+    if choice == "Subtract":
+        result  = fnum - snum
+    if choice == "Multiply":
+        result  = fnum * snum
+    if choice == "Divide":
+        result = fnum / snum
+
+st.success(f"The result is {result}")
+st.balloons()
+st.snow()
