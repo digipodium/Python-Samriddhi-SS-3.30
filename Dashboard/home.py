5d7ab5062ae977943d23994b4bf70e31deca8b54
Areeba Shakeel <shakeelareeba02@gmail.com>
2025-11-02 18:15:35 +0530

    ..

--- Dashboard/home.py
+++ Dashboard/home.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
 st.title("Titanic Dashboard")
+st.markdown("Welcome to the Titanic Dashboard! Use the sidebar to filter the data and explore various visualizations about the Titanic passengers.")
--- Dashboard/pages/dashboard.py
+++ Dashboard/pages/dashboard.py
@@ -7,8 +7,6 @@ df = sns.load_dataset("titanic")
 
 st.title("Explore the insights here👇")
 
-st.dataframe(df)
-
 #filtering options
 st.sidebar.title("Filter Options")
 
@@ -29,19 +27,29 @@ age = st.sidebar.slider("Age Range",
                         value=(int(df["age"].min()), int(df["age"].max()))
                         )
 
+#applying filters
+filtered_df = df.copy()
+if gender:
+    filtered_df = filtered_df[filtered_df["sex"].isin(gender)] #this line filters the dataframe based on selected genders, isin checks if the value is in the list
+if pclass:
+    filtered_df = filtered_df[filtered_df["class"].isin(pclass)]
+if age:     
+    filtered_df = filtered_df[(filtered_df["age"] >= age[0]) & (filtered_df["age"] <= age[1])] #we are using 1 for maximum range, this is the concept of boolean indexing
+st.dataframe(filtered_df)
+
 #histogram of age distribution
-fig = px.histogram(df, x="age", nbins=30, title="Age Distribution")
+fig = px.histogram(filtered_df, x="age", nbins=30, title="Age Distribution")
 st.plotly_chart(fig)
 #conclusion point
 st.markdown("**Conclusion:** The age distribution of Titanic passengers is right-skewed, with a higher concentration of younger passengers. There is a noticeable drop in the number of passengers above 60 years old.")
 
 #bar chart of survival count by passenger class
-fig2 = px.bar(df, x = 'class', y='survived', 
+fig2 = px.bar(filtered_df, x = 'class', y='survived', 
               labels={'index':'Survived', 'survived':'Count'}, 
               title="Survival Count by Passenger Class")
 st.plotly_chart(fig2)
 
 #pie chart of class distribution
-fig3 = px.pie(df, names='class', title="Class Distribution")
+fig3 = px.pie(filtered_df, names='class', title="Class Distribution")
 st.plotly_chart(fig3)
 
